d3a22160eeeb0ed29c417bdb38d225fe1f70edd7
Freddie Burns <freddieburns@hotmail.co.uk>
2024-08-03 19:19:46 +0100

    Create __init__ for plotters module
    This contains a function for plotting a textbox with the theta value that is used by all plotting functions.

---
+++ plotters/__init__.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def theta_textbox(theta, ax=None):
+    """
+    Display value of theta in textbox on plot in terms of pi.
+    """
+    pi_coeff = theta / np.pi
+    label = f"$\\theta$ = {pi_coeff:.2f} $\\pi$"
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+    if ax is None:
+        ax = plt.gca()
+
+    ax.text(
+        0.95, 0.95,
+        label,
+        transform=ax.transAxes,
+        fontsize=14,
+        verticalalignment='top',
+        horizontalalignment='right',
+        bbox=props,
+    )
