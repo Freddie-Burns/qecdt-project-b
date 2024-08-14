96d67a4f0e20f02c9bacb3a44c5be0f93baa367a
Freddie Burns <freddieburns@hotmail.co.uk>
2024-08-14 10:47:09 +0100

    Profile different hadamard methods

--- circuit.py
+++ circuit.py
@@ -1,25 +1,52 @@
 from pprint import pprint
+import itertools
+from time import time
 
+from hadamard_transform import hadamard_transform
+from line_profiler_pycharm import profile
 from sympy import fwht
+from scipy.linalg import hadamard
 import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import pandas as pd
-import seaborn as sns
+import torch
+# import seaborn as sns
 
-sns.set(style="ticks")
+# import graph
+
+# sns.set(style="ticks")
+
+
+@profile
+def main():
+    had_times = []
+    fwht_times = []
+    torch_times = []
+    for n in range(1, 16):
+        network = nx.complete_graph(n)
+        cir = Circuit(network, theta=np.pi/4)
+        h, f, t = cir.prob_distribution()
+        had_times.append(h)
+        fwht_times.append(f)
+        torch_times.append(t)
+    plt.plot(had_times, label="mat mul")
+    plt.plot(fwht_times, label="fhwt")
+    plt.plot(torch_times, label="torch")
+    plt.legend()
+    plt.show()
 
 
 class ColumnHeaders:
     """
     Reference for column headers of DataFrames.
     """
-    outcome = "outcome"
-    bitstring = "bitstring"
-    probability = "probability"
-    theta = "theta"
-    graph_type = "graph type"
-    num_qubits = "number of qubits"
+    outcome = "Outcome"
+    bitstring = "Bitstring"
+    probability = "Probability"
+    theta = "Theta"
+    graph_type = "Graph type"
+    num_qubits = "Number of qubits"
 
 
 class Circuit:
@@ -33,6 +60,7 @@ class Circuit:
 
         self.n = len(network)       # number of vertices / qubits
         self.N = int(2 ** self.n)   # number of possible states / bitstrings
+
         self.psi = self._gen_psi()  # state before final hadamard and mmnt
 
     def _exponent_sum(self, bitstring):
@@ -92,29 +120,83 @@ class Circuit:
         result = np.inner(hadamard_output_complex, self.psi)
         return (np.abs(result) ** 2) / (self.N ** 2)
 
+    @profile
     def prob_distribution(self):
         """
         Return probability of each bitstring outcome.
         """
         outcomes = list(range(self.N))
         bitstrings = [gen_bitstring(i, self.n) for i in outcomes]
-        probabilities = []
         thetas = [self.theta] * self.N
 
-        for i in outcomes:
-            probabilities.append(self.output_prob(i))
+        had_times = []
+        fwht_times = []
+        torch_times = []
 
+        start = time()
+        self.hadamard_mul_prob()
+        had_times.append(time() - start)
+
+        start = time()
+        self.fwht_prob()
+        fwht_times.append(time() - start)
+
+        start = time()
+        self.fwht_prob()
+        torch_times.append(time() - start)
+
+        # start = time()
+        # self.list_comp_prob(outcomes)
+        # list_times.append(time() - start)
+
+        probs = [0] * self.N
         distribution = pd.DataFrame({
             ColumnHeaders.outcome: outcomes,
             ColumnHeaders.bitstring: bitstrings,
-            ColumnHeaders.probability: probabilities,
+            ColumnHeaders.probability: probs,
             ColumnHeaders.theta: thetas,
             ColumnHeaders.graph_type: self.graph_type,
             ColumnHeaders.num_qubits: self.n,
         })
 
-        distribution["probability"] /= distribution["probability"].sum()
-        return distribution
+        # prob_header = ColumnHeaders.probability
+        # distribution[prob_header] /= distribution[prob_header].sum()
+        # return distribution
+
+        return had_times, fwht_times, torch_times
+
+    def hadamard_mul_prob(self):
+        """
+        First method to profile.
+        Calculate Hadamard product of all bitstrings.
+        """
+        hadamard = HadamardSingleton.get(self.N)
+        probs = np.matmul(hadamard, self.psi)
+        return (np.abs(probs) ** 2) / (self.N ** 2)
+
+    def fwht_prob(self):
+        """
+        Second method to profile.
+        Calculate FWHT of all bitstrings.
+        """
+        hadamard_output = fwht(self.psi)
+        result = np.array(hadamard_output, dtype=np.complex128)
+        return (np.abs(result) ** 2) / (self.N ** 2)
+
+    def list_comp_prob(self, outcomes):
+        """
+        Third method to profile.
+        Calculate list of all bitstrings probabilities.
+        """
+        return [self.output_prob(output) for output in outcomes]
+
+    def torch_prob(self):
+        """
+        Fourth method to profile.
+        Calculate pytorch FWHT of all bitstrings.
+        """
+        result = hadamard_transform(self.psi)
+        return (np.abs(result) ** 2) / (self.N ** 2)
 
     @property
     def theta(self):
@@ -132,6 +214,24 @@ class Circuit:
         self.psi = self._gen_psi()
 
 
+class HadamardSingleton:
+    """
+    Prevent repeated calculation of large Hadamard matrices.
+    """
+    matrix = {}
+
+    @classmethod
+    def get(cls, N):
+        """
+        Return the hadamard of size N x N.
+        """
+        if N in cls.matrix.keys():
+            return cls.matrix[N]
+        else:
+            cls.matrix[N] = hadamard(N)
+            return cls.matrix[N]
+
+
 def gen_prob_dataframe():
     """
     Create blank dataframes for probability distributions, n bit values.
@@ -145,3 +245,7 @@ def gen_bitstring(x, n):
     """
     # Pad with leading zeroes to ensure length n
     return bin(x)[2:].zfill(n)
+
+
+if __name__ == "__main__":
+    main()
